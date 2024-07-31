c90d82555adb288cd3c76774b878e66100635a6c
octosapien <user3.insti@gmail.com>
2024-07-31 20:24:55 +0530

    Configured MongoDB URI from environment variable-1

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    const dbURI = process.env.MONGODB_URI;
-    await mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log("MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log('MongoDB connected...');
   } catch (err) {
-    console.error("MongoDB connection error:", err);
-    process.exit(1); // Exit process with failure
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
