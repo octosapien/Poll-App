3cb4a8369ed6be686e40daeba4306673c1555232
octosapien <user3.insti@gmail.com>
2024-07-31 20:44:55 +0530

    Configured MongoDB URI from environment3

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+console.log('MONGO_URI:', process.env.MONGO_URI);
+
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
